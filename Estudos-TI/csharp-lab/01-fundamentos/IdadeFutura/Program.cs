988da013a87837f3b100feea043be25bc3e14c8e
Bield_04 <gabrielbarretodasilva120@gmail.com>
2026-09-14 18:06:41 -0300

    Add files via upload

---
+++ Estudos-TI/csharp-lab/01-fundamentos/IdadeFutura/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Digite a sua Idade:");
+string input = Console.ReadLine();
+int idade = int.Parse(input);
+Console.WriteLine($"Você tem {idade} anos e daqui a 5 anos você tera {idade + 5} anos e daqui a 10 anos você tera {idade + 10} anos e daqui a 20 anos você tera {idade + 20}");
+
---
+++ Estudos-TI/csharp-lab/01-fundamentos/Temperatura/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Digite uma temperatura em celcius para que o programa converta em Fareheints e em Kelvins:");
+string? input = Console.ReadLine();
+double C = int.Parse(input ?? "0");
+double K = C + 273.15;
+double
+F = (C * 1.8) + 32;
+Console.WriteLine($"Você Digitou {C}° celcius e convertendo em Kelvins fica {K} K e em Fahrenheit fica {F} °F");
+
